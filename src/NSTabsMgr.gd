f15e8e5f64feffe8fee8c467d9912a4044760536
Neruthes <i@neruthes.xyz>
2024-08-18 16:43:39 +0000

    Regular update '2024-08-18T16:43:39+00:00'

--- src/NSTabsMgr.gd
+++ src/NSTabsMgr.gd
@@ -3,6 +3,7 @@ extends Node
 
 @export var buttons: Array[Button] = []
 @export var contents: Array[Control] = []
+@export var is_active: bool = true # TODO: If true, listen to game controller switching (L1/R1, etc)
 
 @onready var btn_group: ButtonGroup = ButtonGroup.new()
 
@@ -10,10 +11,31 @@ func _ready() -> void:
 	for btn: Button in buttons:
 		btn.toggle_mode = true
 		btn.button_group = btn_group
-		btn.button_up.connect(_on_tab_button_up)
+		btn.button_up.connect(flush_view_visibility)
+	flush_view_visibility()
 
-func _on_tab_button_up() -> void:
+var _cached_active_index: int = -99
+func flush_view_visibility() -> void:
 	var btn: Button = btn_group.get_pressed_button()
 	var active_index: int = buttons.find(btn)
-	for view in contents:
-		view.visible = contents.find(view) == active_index
+	var is_changed: bool = _cached_active_index != active_index
+	_cached_active_index = active_index
+	if is_changed:
+		for view in contents:
+			view.visible = contents.find(view) == active_index
+
+
+### TODO: Listen to game controller L1/R1
+#func _physics_process(delta: float) -> void:
+	#if Input.is_action_just_pressed("gc_l1"):
+		#try_prev_tab()
+		#return
+	#if Input.is_action_just_pressed("gc_r1"):
+		#try_next_tab()
+		#return
+#
+#func try_prev_tab() -> void:
+	#pass
+#
+#func try_next_tab() -> void:
+	#pass
