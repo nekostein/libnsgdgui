95d6d8bac8fe1d1536c0db7bf4ab1d8bca597bee
Neruthes <i@neruthes.xyz>
2024-08-18 17:50:13 +0000

    Regular update '2024-08-18T17:50:13+00:00'

--- src/NSTabsMgr.gd
+++ src/NSTabsMgr.gd
@@ -1,23 +1,28 @@
 class_name NSTabsMgr
 extends Node
 
-@export var buttons: Array[Button] = []
+@export var buttons: Array[Control] = []
 @export var contents: Array[Control] = []
 @export var is_active: bool = true # TODO: If true, listen to game controller switching (L1/R1, etc)
 
-@onready var btn_group: ButtonGroup = ButtonGroup.new()
+var btn_group: ButtonGroup = ButtonGroup.new()
+
+var _first_real_btn: Button = null
 
 func _ready() -> void:
-	for btn: Button in buttons:
-		btn.toggle_mode = true
-		btn.button_group = btn_group
-		btn.button_up.connect(flush_view_visibility)
+	for real_btn in buttons:
+		if !_first_real_btn:
+			_first_real_btn = real_btn
+			_first_real_btn.set_pressed_no_signal(true)
+		real_btn.toggle_mode = true
+		real_btn.button_group = btn_group
+		real_btn.button_up.connect(flush_view_visibility)
 	flush_view_visibility()
 
 var _cached_active_index: int = -99
 func flush_view_visibility() -> void:
-	var btn: Button = btn_group.get_pressed_button()
-	var active_index: int = buttons.find(btn)
+	var real_btn: Button = btn_group.get_pressed_button()
+	var active_index: int = buttons.find(real_btn)
 	var is_changed: bool = _cached_active_index != active_index
 	_cached_active_index = active_index
 	if is_changed:
